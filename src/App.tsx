e6e8626f53bcecab67c2bcee4ff09398263695b4
Ben Murray <benmurray573@gmail.com>
2022-11-09 15:59:08 +0000

    app complete with map

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { greet } from "./utils/greet";
-import EmojiPicker from "./components/EmojiPicker"
+import EmojiPicker from "./components/EmojiPicker";
 
 function App(): JSX.Element {
-  return <>
-  <h1>{greet("World")}</h1>
-  <EmojiPicker />
-  </>
+  return (
+    <>
+      <h1>{greet("World")}</h1>
+      <EmojiPicker />
+    </>
+  );
 }
 
 export default App;
--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -1,40 +1,51 @@
 import { useState } from "react";
 
 export default function EmojiPicker(): JSX.Element {
-    let [emojiOneFromCurrentRender, queueRerenderWithNewEmoji] = useState("⏰");
-    let [emojiTwoFromCurrentRender, queueRerenderWithNewEmoji] = useState("🛌");
+  const [emojiListCurrentRender, queueRerenderWithNewEmojiList] = useState<
+    string[]
+  >([]);
 
-    const fryingPanSelected = () => {
-        queueRerenderWithNewEmoji(emojiTwoFromCurrentRender = emojiOneFromCurrentRender)
-        queueRerenderWithNewEmoji(emojiOneFromCurrentRender = "🍳")
-    }
-    const alarmClockSelected = () => {
-        queueRerenderWithNewEmoji(emojiTwoFromCurrentRender = emojiOneFromCurrentRender)
-        queueRerenderWithNewEmoji(emojiOneFromCurrentRender = "⏰")
-    }
-    const bedSelected = () => {
-        queueRerenderWithNewEmoji(emojiTwoFromCurrentRender = emojiOneFromCurrentRender)
-        queueRerenderWithNewEmoji(emojiOneFromCurrentRender = "🛌")
-    }
-    const yummyFaceSelected = () => {
-        queueRerenderWithNewEmoji(emojiTwoFromCurrentRender = emojiOneFromCurrentRender)
-        queueRerenderWithNewEmoji(emojiOneFromCurrentRender = "😋")
-    }
-    const dinnerPlateSelected = () => {
-        queueRerenderWithNewEmoji(emojiTwoFromCurrentRender = emojiOneFromCurrentRender)
-        queueRerenderWithNewEmoji(emojiOneFromCurrentRender = "🍽️")
-    }
+  const fryingPanSelected = () => {
+    if (emojiListCurrentRender.length <= 4) {
+      queueRerenderWithNewEmojiList([...emojiListCurrentRender, "🍳"]);
+    } else <p>"You can't have more than five emojis!"</p>;
+  };
+  const alarmClockSelected = () => {
+    if (emojiListCurrentRender.length <= 4) {
+      queueRerenderWithNewEmojiList([...emojiListCurrentRender, "⏰"]);
+    } else <p>"You can't have more than five emojis!"</p>;
+  };
+  const bedSelected = () => {
+    if (emojiListCurrentRender.length <= 4) {
+      queueRerenderWithNewEmojiList([...emojiListCurrentRender, "🛌"]);
+    } else <p>"You can't have more than five emojis!"</p>;
+  };
+  const yummyFaceSelected = () => {
+    if (emojiListCurrentRender.length <= 4) {
+      queueRerenderWithNewEmojiList([...emojiListCurrentRender, "😋"]);
+    } else <p>"You can't have more than five emojis!"</p>;
+  };
+  const dinnerPlateSelected = () => {
+    if (emojiListCurrentRender.length <= 4) {
+      queueRerenderWithNewEmojiList([...emojiListCurrentRender, "🍽️"]);
+    } else <p>"You can't have more than five emojis!"</p>;
+  };
 
-    return (
+  return (
     <>
-    <h3>Which emojis describe your mood right now?</h3>
-    <p>Your current mood: {emojiOneFromCurrentRender}{emojiTwoFromCurrentRender}</p>
-    <hr />
-    <button onClick={fryingPanSelected}>🍳</button>
-    <button onClick={fryingPanSelected}>⏰</button>
-    <button onClick={fryingPanSelected}>🛌</button>
-    <button onClick={fryingPanSelected}>😋</button>
-    <button onClick={fryingPanSelected}>🍽️</button>
+      <h3>Which emojis describe your mood right now?</h3>
+      <p>
+        Your current mood:
+        {emojiListCurrentRender.map((emoji, i) => {
+          return <li key={i}>{emoji}</li>;
+        })}
+      </p>
+      <hr />
+      <button onClick={fryingPanSelected}>🍳</button>
+      <button onClick={alarmClockSelected}>⏰</button>
+      <button onClick={bedSelected}>🛌</button>
+      <button onClick={yummyFaceSelected}>😋</button>
+      <button onClick={dinnerPlateSelected}>🍽️</button>
     </>
-    )
+  );
 }
